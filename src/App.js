f314c4edfcd1022db47a19c747748073a5624898
gonzalezbri <4717bg@gmail.com>
2023-07-11 17:52:42 -0700

    added sign in modal

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import theme from './styles/theme'
 import AppBar from "./components/appBar";
 import Banner from "./components/banner";
 import PromoBar from "./components/promobar";
-import  Products  from "./components/products/index";
+import Products  from "./components/products/index";
 
 function App (){
     useEffect(()=>{
